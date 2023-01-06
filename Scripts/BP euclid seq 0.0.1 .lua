e1c4204cc13b4936a5deec8a9fbe3ac1dc69038a
bigpumkin <86058322+bigpumkin@users.noreply.github.com>
2023-01-06 19:29:16 +0500

    Update BP euclid seq 0.0.1 .lua

--- Scripts/BP euclid seq 0.0.1 .lua
+++ Scripts/BP euclid seq 0.0.1 .lua
@@ -1,28 +1,50 @@
 function main()
 
-  sel_midi = get_sel_midi_item()
-  grid = reaper.MIDI_GetGrid(sel_midi)
-  num_step = get_num_step(grid)
-
+  sel_midi = get_sel_midi_take()
+  num_step = get_num_step(sel_midi)
+  seq_euclid(sel_midi, num_step, 4)
   
+end
+
+--_______Seq func_______________________________
+function seq_euclid(sel_midi, num_step, num_beat)
+
+  for i = 0, num_step - 1 do
+    
+    multiplied = num_step * i
+    
+    index = multiplied / num_beat
+    note_start = get_note_start(sel_midi, num_step,index)
+    
+    if multiplied % num_beat then 
+    
+      if index == 0 then --If note in zero step
+        
+        reaper.MIDI_InsertNote(sel_midi,0,0,0,50,0,72,30)
+        index = index + 1
+      end
+      
+    end
+    
+    reaper.MIDI_InsertNote(sel_midi,0,0,note_start,600,0,72,30)
+    index = index + 1
+    
+  end
+    
 end
 
 
 --_________________Addiction funct___________________
-function get_sel_midi_item()
+function get_sel_midi_take()
 
   if reaper.CountSelectedMediaItems(0) ~= 0 then
-  
-    for i = 0, reaper.CountSelectedMediaItems(0)-1 do 
     
-      item = reaper.GetSelectedMediaItem(0, i)
-      take_midi = reaper.GetActiveTake(item)
-      
-    end
+    item = reaper.GetSelectedMediaItem(0, 0)
+    take = reaper.GetActiveTake(item)
     
-    if reaper.TakeIsMIDI(take_midi)then 
+    if reaper.TakeIsMIDI(take)then 
     
-      return take_midi
+      return take
       
     else 
     
@@ -35,17 +57,18 @@ function get_sel_midi_item()
 end  
 
 
-function get_num_step(grid)
+function get_num_step(sel_midi)
 
+  grid = reaper.MIDI_GetGrid(sel_midi)
   num_step = 1 / grid * 4
   return num_step
   
 end
 
 
-function get_note_start(index)
-
-  note_start = reaper.BR_GetMidiSourceLenPPQ(sel_midi)/16 * index
+function get_note_start(sel_midi, num_step, index)
+  
+  note_start = reaper.BR_GetMidiSourceLenPPQ(sel_midi)/num_step * index
   
   return note_start
   
